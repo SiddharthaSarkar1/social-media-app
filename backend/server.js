9c1800fd3cf39ec4b660c92deed21e185142d15e
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-08-15 02:54:26 +0530

    added metod to create and delete posts. post controller work in progress.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -20,12 +21,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //to parse req.body
-app.use(express.urlencoded({ extended: true })); // to parse theform data(urlencoded)
+app.use(express.urlencoded({ extended: true })); // to parse the form data(urlencoded)
 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
