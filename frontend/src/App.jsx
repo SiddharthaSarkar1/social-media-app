f5ac5074daef18c547cc180164d8e478620109cd
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-09-28 03:15:10 +0530

    added create post functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         if (!res.ok) {
           throw new Error(data.error || "Something went wrong");
         }
-        console.log("Auth user is here: ", data);
+        // console.log("Auth user is here: ", data);
         return data;
       } catch (error) {
         throw new Error(error);
